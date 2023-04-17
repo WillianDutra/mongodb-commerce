5cdfe8a9911b0136d59b3d912317cd327f6e115d
Willian Dutra <contatowilliandutra@gmail.com>
2023-04-17 18:05:12 -0300

    feat: Retornando lanches que tenham um especifico percentual de proteinas

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,5 +1,12 @@
 db.produtos.find(
-  { "valoresNutricionais.0.quantidade": { $lt: 500 } },
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
+  },
   {
     _id: 0,
     nome: 1,
